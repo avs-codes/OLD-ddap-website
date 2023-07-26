b329e0d9c73ccf6caab50725b93af8f248b70664
graeme120 <graemem2414@gmail.com>
2023-07-26 14:44:24 -0400

    yuppp

--- script.js
+++ script.js
@@ -39,3 +39,40 @@ const getData = async () => {
 
 // Always be sure to invoke your function or nothing will run! :D
 getData();
+
+//The function below creates a <div> for every row in our Google Sheet, or every separate club event
+//the backticks (``) allow us to combine HTML with dynamically pulled in data
+//the words 'event' and 'month' within those square brackets represent what column's data we are putting in the tag
+
+const generateHTML = (rowData) => {
+  return `
+    <div>
+      <h1>${rowData['event']}</h1>
+      <h2>${rowData['month']} ${rowData['date']}</h2>
+      <div>${rowData['description']}</div>
+      <a href="${rowData['link1']}">${rowData['link1']}</a>
+    </div>
+  `;
+};
+
+//The above function renders HTML elements for just one row, but this function asynchronously renders + updates the HTMl to reflect the data of the sheet
+
+const renderData = async () => {
+  const data = await getData();
+  //This defines our output container (where our generated HTML goes on the page) as the element with the ID 'output-container-calendar)
+  const outputContainer = document.getElementById('output-container-calendar');
+
+
+  //This redefines generateHTMl as a sum of all the rows of data in the sheet
+  let generatedHTML = '';
+  data.forEach((rowData) => {
+    generatedHTML += generateHTML(rowData);
+  });
+
+  outputContainer.innerHTML = generatedHTML;
+};
+
+
+// ^What Alex said
+renderData();
+
